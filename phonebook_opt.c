ccad76590b21b664ba5749420e05afdee651a8a7
lankudot <airlanser@gmail.com>
2016-09-26 17:42:46 +0800

    Create jenkins hash function

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "phonebook_opt.h"
+
+static uint32_t jenkins(char *key, size_t len);
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastname[], entry *pHead)
@@ -12,4 +15,20 @@ entry *findName(char lastname[], entry *pHead)
 entry *append(char lastName[], entry *e)
 {
     return NULL;
+}
+
+/* Jenkins's one-at-a-time hash function.
+ */
+static uint32_t jenkins(char *key, size_t len)
+{
+    uint32_t hash = 0;
+    for (int i = 0; i < len; ++i) {
+        hash += key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash >> 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash;
 }
